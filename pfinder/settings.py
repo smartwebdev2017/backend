2cb5be09b2866ec11bf0383706540b23d1734a5e
smartwebdev2017 <bern.oliver.630@yandex.com>
2017-09-28 15:37:05 -0400

    update

--- pfinder/settings.py
+++ pfinder/settings.py
@@ -13,9 +13,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pfinder',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'dev',
+        'PASSWORD': 'pfinder',
+        'HOST': '35.192.81.142',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
